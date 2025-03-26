906192bd6ba4f16853d52fe9ee3e743761dfd066
RafalZ <123420024+Protistd@users.noreply.github.com>
2025-03-26 13:59:23 +0100

    Delete pogoda directory

--- pogoda/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.pogoda {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.json;
-
-
-    opens com.example.pogoda to javafx.fxml;
-    exports com.example.pogoda;
-}
